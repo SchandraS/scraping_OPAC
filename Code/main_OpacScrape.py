8359447ef84de208aa2872b3fdcdc89842256703
SchandraS <39059574+SchandraS@users.noreply.github.com>
2021-10-13 00:46:41 +0530

    removing unused code

--- Code/main_OpacScrape.py
+++ Code/main_OpacScrape.py
@@ -6,34 +6,19 @@ Created on Tue Oct 12 00:48:07 2021
 """
 
 import scrapingLIBSYS as scrp
-import datetime # for adding time
-import pytz # for timezone()
-# import os
-# import shutil
-
-def getTime(): # function to get current time in IST
-	# using now() to get current time
-	return datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
-
-"""
-"""
-# Creating Directory
-scrp.CreateData_dir()
-
-#Opening a log file
-log = open(scrp.headerVar.dataPath + "\\log.txt",'a')
-
-def piper(S): #Prints the output to log file & stdout(terminal)
-	global log
-	log.write(S)
-	print(S)
-
-#set print commands in scrapingLIBSYS to use piper()
-scrp.print = piper
 
 
 # Main Function
 if __name__ == "__main__" :
+
+	#set print commands in scrapingLIBSYS to use piper() in ScrapeLogger
+	scrp.print = scrp.scplg.piper
+
+	#Open Log file
+	scrp.scplg.openLog()
+
+	# Creating Data Directory
+	scrp.CreateData_dir()
 
 	#UID setup
 	UID_start = 2018122001 #first roll number
@@ -44,23 +29,14 @@ if __name__ == "__main__" :
 
 
 	for UID in UID_range: #Iterate though UIds
-		#piper("#Scraping started at IST:" + str(getTime()) + "\nFor User: " + str(UID) +'\n')
-
 		#Scraping Data for UID
 		status_Scrape = scrp.ScrapeUserData(str(UID))
 
 		if status_Scrape == scrp.err.SITE_DOWN: #if website is down
 			print("!!!Terminal Message: Website is Down")
 			break
-		elif status_Scrape == scrp.err.INVALID_UID : #Invalid User ID
-# 			piper("~~ UID Invalid, Deleting Directory for: " + str(UID)) #Log & Print
-# 			UID_dir = os.path.join(scrp.headerVar.dataPath, str(UID)) #Path to UID directory
-# 			shutil.rmtree(UID_dir, ignore_errors = False) #Delete Directory and FIles
-# 			piper("~~ Directory Deleted:" + str(UID)) #Log & Print #Directory Deleted
-			pass
+		elif status_Scrape == scrp.err.INVALID_UID : #Invalid User ID, delete directory
+			scrp.DeleteUser_dir(UID)
 
-
-# 	#Closing Log File
-# 	piper("\n#Log File Closed at: " + str(getTime()) + "\n\n")
-# 	log.close()
-# 	print("\n\n!!!Log file closed {Message after running log.close()}")
+	#Closing Log File
+	scrp.scplg.closeLog()
