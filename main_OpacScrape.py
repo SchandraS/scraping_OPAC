239cf833556f168cc85e4b7418e22aa4af95f020
SchandraS <39059574+SchandraS@users.noreply.github.com>
2021-10-12 20:52:20 +0530

    Update main_OpacScrape.py

--- main_OpacScrape.py
+++ main_OpacScrape.py
@@ -8,13 +8,17 @@ Created on Tue Oct 12 00:48:07 2021
 import scrapingLIBSYS as scrp
 import datetime # for adding time
 import pytz # for timezone()
-    
+import ErrorCode_scrapingLIBSYS as err
 
 def getTime(): # function to get current time in IST
 	# using now() to get current time
 	return datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
 
-#OPening a log file
+
+# Creating Directory
+scrp.CreateData_dir()
+
+#Opening a log file
 log = open(scrp.headerVar.dataPath + "\\log.txt",'a')
 
 def piper(S): #Prints the output to log file & stdout(terminal)
@@ -27,17 +31,23 @@ scrp.print = piper
 
 #UID setup
 UID_start = 2018122001 #first roll number
-UID_end = 2018122012 #last roll number
+UID_end   = 2018122012 #last roll number
 
+#UID Range
 UID_range = range(UID_start,UID_end+1)
 
 if __name__ == "__main__" : # Main Function
 	for UID in UID_range: #Iterate though UIds
 		piper("#Scraping started at IST:" + str(getTime()) + "\nFor User:" + str(UID) +'\n')
-		scrp.CreateData_dir() # Creating Directory
-		scrp.ScrapeUserData(str(UID)) #Scrping UID
 
-#Closing Log File
-piper("\n#Log File Closed at:" + str(getTime()) + "\n\n")
-log.close()
-print("\n\n!!!Log file closed {Message after running log.close()}")
+		#Scraping Data for UID
+		status_Scrape = scrp.ScrapeUserData(str(UID))
+		if status_Scrape == err.SITE_DOWN:
+			print("!!!Terminal Message: Website is Down")
+			break
+
+
+	#Closing Log File
+	piper("\n#Log File Closed at:" + str(getTime()) + "\n\n")
+	log.close()
+	print("\n\n!!!Log file closed {Message after running log.close()}")
