f1ee413586fa35452cd2c8559d1d277e89dc671c
SchandraS <39059574+SchandraS@users.noreply.github.com>
2021-10-12 23:52:48 +0530

    minor edits

--- Code/ScrapeLogger.py
+++ Code/ScrapeLogger.py
@@ -10,7 +10,7 @@ import datetime # for adding time
 import pytz # for timezone()
 
 
-
+# log = 0
 
 
 def getTime(): # function to get current time in IST
@@ -18,18 +18,22 @@ def getTime(): # function to get current time in IST
 	return datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
 
 def openLog():#Opening a log file
-	return open(headerVar.logFile,'a') #return file variable
+	global log
+	log =  open(headerVar.logFile,'a') #return file variable
+	piper("\n#Log File Opened at:" + str(getTime()) + "\n\n")
+	return log
 
 def piper(S): #Prints the output to log file & stdout(terminal)
 	log = openLog()
-	global log
+# 	global log
 	log.write(S)
 	print(S)
 
 def closeLog(): #Closing Log File
+	global log
 	piper("\n#Log File Closed at:" + str(getTime()) + "\n\n")
 	log.close()
 	print("\n\n!!!Log file closed {Message after running log.close()}")
 
 #set print commands in scrapingLIBSYS to use piper()
-scrp.print = piper
+# scrp.print = piper
