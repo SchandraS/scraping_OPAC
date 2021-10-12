d78660169e728edd7e6471b1972c33afd974c857
SchandraS <39059574+SchandraS@users.noreply.github.com>
2021-10-12 18:47:19 +0530

    removed old code

--- main_OpacScrape.py
+++ main_OpacScrape.py
@@ -6,25 +6,18 @@ Created on Tue Oct 12 00:48:07 2021
 """
 
 import scrapingLIBSYS as scrp
+
 log = open(scrp.headerVar.dataPath + "\\log.txt",'a')
-f = open(scrp.headerVar.dataPath + "\\logger.txt",'a')
-#print = f.write = log.write
-#L = ["hellow1",'HEDHED']
-#f.writelines(L)
 
 def piper(S): #Pipes the output two file
-	global f
-	f.write(S)
 	global log
 	log.write(S)
-	print(S)
+	scrp.print
 
 
-scrp.print = piper
+scrp.print = piper #set print commands in scrapingLIBSYS to use piper()
 
 if __name__ == "__main__" : # Main Function
 	scrp.CreateData_dir() # Creating Directory
-	#print("\nhello2")
-	#piper("\nhello5")
+	print("\nhello2")
 	log.close()
-	f.close()
