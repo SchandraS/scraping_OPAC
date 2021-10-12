11d3eff089b6e8a03e2eac1286e68026f7225148
SchandraS <39059574+SchandraS@users.noreply.github.com>
2021-10-12 20:29:20 +0530

    UId iterator setup

--- main_OpacScrape.py
+++ main_OpacScrape.py
@@ -25,11 +25,17 @@ def piper(S): #Prints the output to log file & stdout(terminal)
 #set print commands in scrapingLIBSYS to use piper()
 scrp.print = piper
 
+#UID setup
+UID_start = 2018122001 #first roll number
+UID_end = 2018122012 #last roll number
+
+UID_range = range(UID_start,UID_end+1)
+
 if __name__ == "__main__" : # Main Function
-	UID = "2018122009"
-	piper("#Scraping started at IST:" + str(getTime()) + "\nFor User:" + UID +'\n')
-	scrp.CreateData_dir() # Creating Directory
-	scrp.ScrapeUserData(UID) #Scrping UID
+	for UID in UID_range: #Iterate though UIds
+		piper("#Scraping started at IST:" + str(getTime()) + "\nFor User:" + str(UID) +'\n')
+		scrp.CreateData_dir() # Creating Directory
+		scrp.ScrapeUserData(str(UID)) #Scrping UID
 
 #Closing Log File
 piper("\n#Log File Closed at:" + str(getTime()) + "\n\n")
