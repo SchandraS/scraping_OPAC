3024ed7b9256f9bb8f6df40d48494a345c852c00
SchandraS <39059574+SchandraS@users.noreply.github.com>
2021-10-12 18:56:21 +0530

    Update main_OpacScrape.py

--- main_OpacScrape.py
+++ main_OpacScrape.py
@@ -6,18 +6,25 @@ Created on Tue Oct 12 00:48:07 2021
 """
 
 import scrapingLIBSYS as scrp
+import datetime # for adding time
+import pytz # for timezone()
+    
+# using now() to get current time 
+current_time = datetime.datetime.now(pytz.timezone('Asia/Kolkata')) 
 
+#OPening a log file
 log = open(scrp.headerVar.dataPath + "\\log.txt",'a')
 
-def piper(S): #Pipes the output two file
+def piper(S): #Prints the output to log file & stdout(terminal)
 	global log
 	log.write(S)
-	scrp.print
-
+	scrp.print(S)
 
 scrp.print = piper #set print commands in scrapingLIBSYS to use piper()
 
 if __name__ == "__main__" : # Main Function
 	scrp.CreateData_dir() # Creating Directory
-	print("\nhello2")
+	#print("\nhello2")
+	UID = "2018122009"
+	scrp.ScrapeUserData(UID)
 	log.close()
