310225a8859f3097e01a2b7c5f2234c97a1dad56
SchandraS <39059574+SchandraS@users.noreply.github.com>
2021-10-12 19:06:10 +0530

    function to add time of scraping
    added timing function

--- main_OpacScrape.py
+++ main_OpacScrape.py
@@ -9,8 +9,10 @@ import scrapingLIBSYS as scrp
 import datetime # for adding time
 import pytz # for timezone()
     
-# using now() to get current time 
-current_time = datetime.datetime.now(pytz.timezone('Asia/Kolkata')) 
+
+def getTime(): # function to get current time in IST
+	# using now() to get current time
+	return datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
 
 #OPening a log file
 log = open(scrp.headerVar.dataPath + "\\log.txt",'a')
@@ -18,7 +20,7 @@ log = open(scrp.headerVar.dataPath + "\\log.txt",'a')
 def piper(S): #Prints the output to log file & stdout(terminal)
 	global log
 	log.write(S)
-	scrp.print(S)
+	print(S)
 
 scrp.print = piper #set print commands in scrapingLIBSYS to use piper()
 
@@ -26,5 +28,7 @@ if __name__ == "__main__" : # Main Function
 	scrp.CreateData_dir() # Creating Directory
 	#print("\nhello2")
 	UID = "2018122009"
+	piper("Scraping started at IST:" + str(getTime()) + "\nFor User:" + UID)
 	scrp.ScrapeUserData(UID)
 	log.close()
+	piper("\nLog File Closed at:" + str(getTime()) + "\n\n")
