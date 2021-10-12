424c97efed8c3c313d4e64b640c4d78aa8e23caf
SchandraS <39059574+SchandraS@users.noreply.github.com>
2021-10-12 17:42:36 +0530

    trying to pipe print o std.out and file

--- main_OpacScrape.py
+++ main_OpacScrape.py
@@ -6,6 +6,14 @@ Created on Tue Oct 12 00:48:07 2021
 """
 
 import scrapingLIBSYS as scrp
+log = open(scrp.headerVar.dataPath + "\\log.txt",'a')
+f = open(scrp.headerVar.dataPath + "\\logger.txt",'a')
+#print = f.write = log.write
+#L = ["hellow1",'HEDHED']
+#f.writelines(L)
 
 if __name__ == "__main__" : # Main Function
 	scrp.CreateData_dir() # Creating Directory
+	print("\nhello2")
+	log.close()
+	f.close()
