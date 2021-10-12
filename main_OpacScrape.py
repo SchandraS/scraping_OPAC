ad971581bc3a622268c50331e774ecb80c16acab
SchandraS <39059574+SchandraS@users.noreply.github.com>
2021-10-12 21:44:03 +0530

    Update main_OpacScrape.py

--- main_OpacScrape.py
+++ main_OpacScrape.py
@@ -8,7 +8,8 @@ Created on Tue Oct 12 00:48:07 2021
 import scrapingLIBSYS as scrp
 import datetime # for adding time
 import pytz # for timezone()
-import ErrorCode_scrapingLIBSYS as err
+import os
+import shutil
 
 def getTime(): # function to get current time in IST
 	# using now() to get current time
@@ -29,28 +30,32 @@ def piper(S): #Prints the output to log file & stdout(terminal)
 #set print commands in scrapingLIBSYS to use piper()
 scrp.print = piper
 
-#UID setup
-UID_start = 2018122001 #first roll number
-UID_end   = 2018122012 #last roll number
 
-#UID Range
-UID_range = range(UID_start,UID_end+1)
+# Main Function
+if __name__ == "__main__" :
 
-if __name__ == "__main__" : # Main Function
+	#UID setup
+	UID_start = 2018122001 #first roll number
+	UID_end   = 2018122012 #last roll number
+	
+	#UID Range
+	UID_range = range(UID_start,UID_end+1)
+
+
 	for UID in UID_range: #Iterate though UIds
-		piper("#Scraping started at IST:" + str(getTime()) + "\nFor User:" + str(UID) +'\n')
+		piper("#Scraping started at IST:" + str(getTime()) + "\nFor User: " + str(UID) +'\n')
 
 		#Scraping Data for UID
 		status_Scrape = scrp.ScrapeUserData(str(UID))
 
-		if status_Scrape == err.SITE_DOWN: #if website is down
+		if status_Scrape == scrp.err.SITE_DOWN: #if website is down
 			print("!!!Terminal Message: Website is Down")
 			break
-		elif status_Scrape == err.INVALID_UID : #Invalid User ID
-			piper("~~ UID Invalid, Deleting Directory for" + str(UID)) #Log & Print
-			pass #Delete The directory
+		elif status_Scrape == scrp.err.INVALID_UID : #Invalid User ID
+			piper("~~ UID Invalid, Deleting Directory for: " + str(UID)) #Log & Print
+			UID_dir = os.path.join(scrp.headerVar.dataPath, str(UID)) #Path to UID directory
+			shutil.rmtree(UID_dir, ignore_errors = False) #Delete Directory and FIles
 			piper("~~ Directory Deleted:" + str(UID)) #Log & Print #Directory Deleted
-
 
 
 	#Closing Log File
