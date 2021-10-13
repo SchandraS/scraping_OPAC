c77c7a9f5483a6fd2819a32c4d9fd73832c91c22
SchandraS <39059574+SchandraS@users.noreply.github.com>
2021-10-13 11:02:08 +0530

    nochange - new login range

--- Code/main_OpacScrape.py
+++ Code/main_OpacScrape.py
@@ -18,7 +18,7 @@ if __name__ == "__main__" :
 	scrp.CreateData_dir()
 
 	#UID setup
-	UID_start = 2018000001 #first roll number
+	UID_start = 2018006928 #first roll number
 	UID_end   = 2018999999 #last roll number
 	
 	#UID Range
